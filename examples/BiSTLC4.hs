623bd1f33b6f3d860c6af5ac6d0c4587181e0e41
Oleg Grenrus <oleg.grenrus@iki.fi>
2021-07-12 04:28:34 +0300

    WIP

--- examples/BiSTLC4.hs
+++ examples/BiSTLC4.hs
@@ -26,9 +26,11 @@ import Pretty
 -- Types
 -------------------------------------------------------------------------------
 
+type TyVar = ShortText
+
 -- | Types.
 data Ty
-    = Ty ShortText
+    = Ty TyVar
     | TUnit
     | Ty :*: Ty
     | Ty :-> Ty
@@ -60,6 +62,9 @@ data Elim a
 
     -- annotated term
     | Ann (Term a) Ty
+
+    -- eta-expand me
+    | Eta Ty (Term a)
   deriving (Functor, Foldable, Traversable)
 
 (.:) :: Term a -> Ty -> Elim a
@@ -76,6 +81,9 @@ data Term a
 
     -- :*: Introduction
     | Mul (Term a) (Term a)
+
+    -- Unit ty introduction
+    | Uno
 
   deriving (Functor, Foldable, Traversable)
 
@@ -95,6 +103,7 @@ instance Monad Elim where
 
     Var x      >>= k = k x
     Ann x t  >>= k = Ann (x >>== k) t
+    Eta t x  >>= k = Eta t (x >>== k)
     App f x  >>= k = App (f >>= k) (x >>== k)
     Fst x    >>= k = Fst (x >>= k)
     Snd x    >>= k = Snd (x >>= k)
@@ -103,6 +112,7 @@ instance Module Term Elim where
     Emb x    >>== k = Emb (x >>= k)
     Lam b    >>== k = Lam (b >>== k)
     Mul x y  >>== k = Mul (x >>== k) (y >>== k)
+    Uno      >>== _ = Uno
 
 instance LiftedModule Term Elim where
     mlift = Emb
@@ -136,6 +146,10 @@ pprElim (Ann x t) = do
     x' <- pprTerm x
     t' <- ppr t
     return $ sexpr (text "the") [t', x']
+pprElim (Eta t x) = do
+    x' <- pprTerm x
+    t' <- ppr t
+    return $ sexpr (text "eta") [t', x']
 pprElim (Fst x)  = do
     x' <- pprElim x
     return $ sexpr (text "fst") [x']
@@ -144,7 +158,9 @@ pprElim (Snd x)  = do
     return $ sexpr (text "snd") [x']
 
 pprTerm :: Term Doc -> PrettyM  Doc
-pprTerm (Emb e) = pprElim e
+pprTerm (Emb e) = do
+    e' <- pprElim e
+    return $ sexpr (text "emb") [e']
 pprTerm (Lam b) = do
     n <- text <$> fresh "x"
     b' <- pprTerm (instantiate1H (Var n) b)
@@ -153,6 +169,7 @@ pprTerm (Mul x y) = do
     x' <- pprTerm x    
     y' <- pprTerm y
     return $ sexpr (text "pair") [x', y']
+pprTerm Uno = return (text "uno")
 
 -- We output
 --   (0 1 2 3)
@@ -192,6 +209,7 @@ data NFTerm a
     = NFEmb (UNeut a)
     | NFLam (ScopeH () NFTerm NFElim a)
     | NFMul (NFTerm a) (NFTerm a)
+    | NFUno
   deriving (Functor, Foldable, Traversable)
 
 -- | Upsilon neutral eliminations
@@ -200,6 +218,7 @@ data UNeut a
     | NFApp (BNeut a) (NFTerm a)
     | NFFst (BNeut a)
     | NFSnd (BNeut a)
+    | NFEta TyVar (NFTerm a)
     | NFEvalPanic
   deriving (Functor, Foldable, Traversable)
 
@@ -246,6 +265,25 @@ nfEmb :: NFElim a -> NFTerm a
 nfEmb (NFElimNeu u) = NFEmb u
 nfEmb (NFElimAnn t _) = t -- upsilon-reduction
 
+nfEtaTy :: TyVar -> NFTerm a -> NFElim a
+nfEtaTy ty t = NFElimNeu (NFEta ty t)
+
+nfEta :: Ty -> NFTerm a -> NFElim a
+nfEta TUnit        _ = NFElimAnn NFUno TUnit
+nfEta (Ty ty)      t = nfEtaTy ty t
+nfEta ty@(a :*: b) t =
+    nfAnn (NFMul
+        (nfEmb (nfEta a (nfEmb (nfFst e))))
+        (nfEmb (nfEta b (nfEmb (nfSnd e))))) ty
+  where
+    e = nfAnn t ty
+nfEta ty@(_ :-> b) t =
+    nfAnn (NFLam (toScopeH (nfEmb (nfEta b (nfEmb (nfApp
+      (fmap F f)
+      (nfEmb (NFElimNeu (NFVar (B ())))))))))) ty
+  where
+    f = nfAnn t ty
+
 instance Applicative NFElim where
     pure = nfVar
     (<*>) = ap
@@ -261,6 +299,7 @@ substU (NFVar x)   k = k x
 substU (NFApp f s) k = nfApp (substB f k) (s >>== k)
 substU (NFFst e)   k = nfFst (substB e k)
 substU (NFSnd e)   k = nfSnd (substB e k)
+substU (NFEta t s) k = nfEtaTy t (s >>== k)
 substU NFEvalPanic _ = NFElimNeu NFEvalPanic
 
 substB :: BNeut a -> (a -> NFElim b) -> NFElim b
@@ -271,6 +310,7 @@ instance Module NFTerm NFElim where
     NFEmb u   >>== k = nfEmb (substU u k)
     NFLam b   >>== k = NFLam (b >>== k)
     NFMul t s >>== k = NFMul (t >>== k) (s >>== k)
+    NFUno     >>== _ = NFUno
 
 -------------------------------------------------------------------------------
 -- From normal forms to terms
@@ -291,16 +331,18 @@ instance ToElim BNeut where
     toElim (BNeutAnnEmb e ty) = Ann (Emb (toElim e)) ty
 
 instance ToElim UNeut where
-    toElim (NFVar a)   = Var a
-    toElim (NFApp f s) = App (toElim f) (toTerm s)
-    toElim (NFFst e)   = Fst (toElim e)
-    toElim (NFSnd e)   = Snd (toElim e)
-    toElim NFEvalPanic = error "eval panic"
+    toElim (NFVar a)    = Var a
+    toElim (NFApp f s)  = App (toElim f) (toTerm s)
+    toElim (NFFst e)    = Fst (toElim e)
+    toElim (NFSnd e)    = Snd (toElim e)
+    toElim (NFEta ty t) = Eta (Ty ty) (toTerm t)
+    toElim NFEvalPanic  = error "eval panic"
 
 instance ToTerm NFTerm where
     toTerm (NFEmb e)   = Emb (toElim e)
     toTerm (NFLam t)   = Lam (toScopeH (toTerm (fromScopeH t)))
     toTerm (NFMul t s) = Mul (toTerm t) (toTerm s)
+    toTerm NFUno       = Uno
 
 
 -------------------------------------------------------------------------------
@@ -318,6 +360,9 @@ infer' (Var (a, at)) = Just (nfVar a, at)
 infer' (Ann x t) = do
     x' <- check' x t
     Just (nfAnn x' t, t)
+infer' (Eta t x) = do
+    x' <- check' x t
+    Just (nfEta t x', t)
 infer' (App f x) = do
     (f', ft) <- infer' f
     case ft of
@@ -356,6 +401,9 @@ check' (Mul x y) t = case t of
         y' <- check' y b
         return (NFMul x' y')
     _ -> Nothing
+check' Uno t = case t of
+    TUnit -> return NFUno
+    _     -> Nothing
 
 -------------------------------------------------------------------------------
 -- Examples
@@ -382,12 +430,18 @@ demo name e = case infer ctx e of
     ctx "ac2d" = "A" :-> "C" :-> "D"
     ctx "bc2d" = "B" :-> "C" :-> "D"
     ctx "aa2b" = "A" :-> "A" :-> "B"
+    ctx "triple" = tripleTy
     ctx _     = TUnit
+
+tripleTy :: Ty
+tripleTy = "A" :*: ("B" :*: ("C" :*: TUnit))
 
 tests :: TestTree
 tests = testGroup "Bi-directional STLC 4"
     [ demo' "arr-beta"  $ (lam_ "x" ("f" $$ "x") .: "A" :-> "B") $$ "a"
     , demo' "pair-beta" $ Fst (Mul "a" "b" .: "A" :*: "B")
+    , demo' "eta-triple" $ Eta tripleTy "triple"
+    , demo' "eta-fun"    $ Eta ("B" :-> "C" :-> "D") "bc2d"
     ]
   where
     demo' name e = goldenVsString name ("examples" </> name' -<.> "txt")
